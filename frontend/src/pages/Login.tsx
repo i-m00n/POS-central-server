b11c09d97d6322cc31db8a7682af070e4321ba9e
I-Man104 <ayman.m.h104@gmail.com>
2024-12-02 13:45:17 +0200

    update

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,49 +1,47 @@
 // src/pages/Login.tsx
-import { useState } from 'react';
+import { useState } from "react";
 
 function Login({ onLogin }: { onLogin?: () => void }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [alert,setAlert] = useState<boolean>(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [alert, setAlert] = useState<boolean>(false);
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-      fetch("/api/auth/login",
-        {
-          method:"POST",
-          headers:{
-            "Content-Type": "application/json"
-          },
-          body:JSON.stringify({
-            "username": username,
-            "password": password
-        })
-        }
-      ).then(res=>res.json()).then(data=>{
-        if(data.token){
-        localStorage.setItem('authToken', data.token)
-        localStorage.setItem('expiryTime', data.expiresIn)
-        if(onLogin)
-        onLogin(); 
-        }
-        else{
+    fetch("http://localhost:4000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.token) {
+          localStorage.setItem("authToken", data.token);
+          localStorage.setItem("expiryTime", data.expiresIn);
+          if (onLogin) onLogin();
+        } else {
           setAlert(true);
         }
-      }
-    );
+      });
   };
 
   return (
     <div className="login-container flex justify-center items-center h-screen bg-bg-color">
       <form onSubmit={handleSubmit} className="p-8 bg-lighterGray shadow-md rounded">
-      {alert&& 
-        <div className='text-center text-whi flex justify-center'>
-          <p className='bg-light-red p-2 rounded-sm text-xl mb-4'>ادخل كلمة السر الصحيحه!</p></div>
-      }
+        {alert && (
+          <div className="text-center text-whi flex justify-center">
+            <p className="bg-light-red p-2 rounded-sm text-xl mb-4">ادخل كلمة السر الصحيحه!</p>
+          </div>
+        )}
         <h2 className="text-xl text-whi font-bold mb-4">تسجيل الدخول</h2>
         <input
           type="text"
           placeholder="الاسم"
-          
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           className="mb-3 p-2 border rounded w-full bg-gray-100 placeholder:text-paleGray"
@@ -56,7 +54,10 @@ function Login({ onLogin }: { onLogin?: () => void }) {
           onChange={(e) => setPassword(e.target.value)}
           className="mb-3 p-2 border rounded w-full bg-gray-100 placeholder:text-paleGray"
         />
-        <button type="submit" className="w-full p-2 bg-paleOrange hover:bg-darkOrange hover:shadow-md hover:shadow-darkgray transition-all text-white rounded">
+        <button
+          type="submit"
+          className="w-full p-2 bg-paleOrange hover:bg-darkOrange hover:shadow-md hover:shadow-darkgray transition-all text-white rounded"
+        >
           Log In
         </button>
       </form>
